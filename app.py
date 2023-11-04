e451c6dd17cce6abfb403b9a391b547de9c240a7
Akshay Shendre <akshay6720@gmail.com>
2023-11-04 23:07:06 +0530

    css/html

--- app.py
+++ app.py
@@ -4,21 +4,28 @@ from flask import render_template, request, redirect
 from utils import Predict
 
 app = flask.Flask(__name__)
+app.config["debug"] = True
 
 @app.route("/")
 def welcome():
-    return render_template("index.html")
+    return render_template("approved.html")
+@app.route("/home")
+def home():
+    return "Bye"
 
-@app.route("/Predict", methods=['GET',"POST"])
-def predict():
-    if request.method == 'POST':
-        return render_template("home.html")
+# @app.route("/Predict", methods=['GET',"POST"])
+# def predict():
+#     if request.method == 'POST':
+#         return render_template("home.html")
 
-@app.route("/Predict/Approved", methods=["GET","POST"])
-def approved():
-    if request.method =="POST":
-        pred = []
-        if pred == 1:
-            return render_template("approved.html")
-        else:
-            return render_template("notapproved.html")
+# @app.route("/Predict/Approved", methods=["GET","POST"])
+# def approved():
+#     if request.method =="POST":
+#         pred = []
+#         if pred == 1:
+#             return render_template("approved.html")
+#         else:
+#             return render_template("denied.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)
