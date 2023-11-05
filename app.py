148838429261162e6689f77c0a0e5c4c231a22db
Akshay Shendre <akshay6720@gmail.com>
2023-11-05 23:53:24 +0530

    home.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import flask
 import numpy as np
 from flask import render_template, request, redirect
-from utils import Predict
+from utils import Model, Preprocess
 
 app = flask.Flask(__name__)
 app.config["debug"] = True
@@ -20,6 +20,7 @@ def approved():
         Total_amount = float(request.form['Amount'])
         Term = float(request.form['term'])
         Int_rate = np.round(float(request.form['int_rate']), 2)
+        Installment = float(request.form['installment'])
         Annual_Income = float(request.form['annual_inc'])
         Dti = float(request.form['dti'])
         Open_account = float(request.form['open_acc'])
@@ -35,14 +36,22 @@ def approved():
         Initial_list_status = request.form['initial_list_status']
         Purpose = request.form['purpose']
         Home_ownership = request.form['home_ownership']
-        Zipcode = request.form['zipcode']
-        earliest_cr_year = int(request.form['earliest_cr_year'])
-        pred = []
+        Zipcode = int(request.form['zipcode'])
+        Earliest_cr_year = int(request.form['earliest_cr_year'])
+
+        features_list = [Total_amount,  Term, Int_rate, Installment, Annual_Income, Dti, Open_account, Pub_rec, Revol_balance, 
+                         Revol_util, Total_account, Mort_acc, Bankruptcies, Grade, Verification_status, 
+                         Application_type, Initial_list_status, Purpose, Home_ownership, Zipcode, Earliest_cr_year]
+        processed = Preprocess(features_list)
+        X_array = processed.out_array
+        model = Model(X_array)
+        pred = model.predict()
+        pred = np.where(pred >0.5, 1,0)
         if pred == 1:
             return render_template("approved.html")
         else:
             return render_template("denied.html")
-        return "BYE"
+        return "Error"
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5000)
--- utils.py
+++ utils.py
@@ -2,9 +2,84 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from sklearn.preprocessing import StandardScaler
 
-class Predict:
-    def __init__(self,list):
-        with open("/Users/akshayshendre/Desktop/Apploan/models/model.h5", "rb") as file:
-            model = load_model(file)
+
+dic1 = {'A2':False,'A3':False,'A4':False,'A5':False,'B1':False,'B2':False,'B3':False,'B4':False,'B5':False,'C1':False,'C2':False,'C3':False,
+            'C4':False,'C5':False,'D1':False,'D2':False,'D3':False,'D4':False,'D5':False,'E1':False,'E2':False,'E3':False,'E4':False,'E5':False,
+            'F1':False,'F2':False,'F3':False,'F4':False,'F5':False,'G1':False,'G2':False,'G3':False,'G4':False,'G5':False
+            }
+dic2 = {'Source Verified':False,'Verified':False}
+dic3 = {'INDIVIDUAL':False,'JOINT':False}
+dic4 = {"w":False}
+dic5 = {"credit_card":False,"debt_consolidation":False,"educational":False,"home_improvement":False,"house":False,"major_purchase":False,
+        "medical":False,"moving":False, "other":False, "renewable_energy":False,"small_business":False,"vacation":False,"wedding":False }
+dic6 = {'OTHER':False,'OWN':False,'RENT':False}
+
+
+
+
+class Preprocess:
+    def __init__(self, feature_list):
+        self.list = feature_list
+    def out_array(self):
+        output =[]
+        for num in range(0,13):
+            output.append(self.list[num])
+        subgrade = dic1
+        if self.list[13] == "A1":
+            output = output + list(dic1.values())
+        else:
+            subgrade[self.list[13]] =True
+            output = output + list(subgrade.values())
+
+        verification = dic2
+        if self.list[14] == "Not Verified":
+            output = output + list(dic2.values())
+        else:
+            verification[self.list[14]] =True
+            output = output + list(verification.values())
+
+        app_type = dic3
+        if self.list[15] == "DIRECT_PAY":
+            output = output + list(dic3.values())
+        else:
+            verification[self.list[15]] =True
+            output = output + list(app_type.values())
+
+        lis_status = dic4
+        if self.list[16]=='f':
+            output = output + list(dic4.values())
+        else:
+            lis_status[self.list[16]] = True
+            output = output + list(lis_status.values())
+
+        purpose = dic5
+        if self.list[17]=="car":
+            output= output + list(dic5.values())
+        else:
+            purpose[self.list[17]]=True
+            output = output + list(purpose.values())
+
+        ownership = dic6
+        if self.list[18]=="MORTGAGE":
+            output = output + list(dic6.values())
+        else:
+            ownership[self.list[18]]=True
+            output = output + list(ownership.values())
+
+        output = output + list(self.list[19:])
+        output_array = np.array(output)
+        scaler = StandardScaler()
+        output_array = scaler.transform(output_array)
+        return output_array        
+
+class Model:
+    def __init__(self,input_array):
+        self.array = input_array
+    def predict(self):
+        model = load_model("models/model.h5")
+        pred = model.predict(self.array)
+        return pred
+            
     
